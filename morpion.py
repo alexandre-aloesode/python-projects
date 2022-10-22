13d2e7cb9418f3354a9a0a103cd89f0ad1407471
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2022-10-23 01:40:08 +0200

    blocage si case déjà jouée

--- morpion.py
+++ morpion.py
@@ -1,5 +1,7 @@
 board = [["-","-","-"], ["-","-","-"], ["-","-","-"]]
-running = True 
+running = True
+history = []
+check = ""
 
 def board_display():
     for i in range(len(board)):
@@ -60,13 +62,13 @@ def victory2(player2):
         return running == False
 
 def check_input(user):
-    play= input(user1+"joue (ligne puis colonne) : ")
+    play= input(user+" joue (ligne puis colonne) : ")
     while len(play) > 3 or len(play) < 3 :
         print("Choisir un chiffre entre 1 et 3 pour la ligne, ainsi que la colonne, les deux séparés d'un espace\n")
-        play= input("joue (ligne puis colonne) : ")
+        play= input(user+" joue (ligne puis colonne) : ")
     while play != "1 1" and play != "1 2" and play != "1 3" and play !=  "2 1" and play !=  "2 2" and play !=  "2 3" and play !=  "3 1" and play != "3 2" and play != "3 3":
         print("Choisir un chiffre entre 1 et 3 pour la ligne, ainsi que la colonne, les deux séparés d'un espace\n")
-        play = input("joue (ligne puis colonne) : ")
+        play = input(user+" joue (ligne puis colonne) : ")
     return play
 
 
@@ -86,11 +88,20 @@ elif action == "j":
         while running != False:
             
             board_display()
-
+            
             play1, play2 = check_input(user1).split()
             ligne1 = int(play1)
-            colonne1 = int(play2)   
-            board[ligne1-1][colonne1-1] = "X"
+            colonne1 = int(play2)
+            check = 1  
+            while check == 1:
+                if board[ligne1-1][colonne1-1] != "-":
+                    print("ceci a déja été joué\n")
+                    play1, play2 = check_input(user1).split()
+                    ligne1 = int(play1)
+                    colonne1 = int(play2)
+                else:
+                    board[ligne1-1][colonne1-1] = "X"
+                    check = 2          
             running = victory1(board)
             if running == False:
                 ranking(user1)
@@ -100,8 +111,16 @@ elif action == "j":
             play3, play4 = check_input(user2).split()
             ligne2 = int(play3)
             colonne2 = int(play4)
-            board[ligne2-1][colonne2-1] = "O"
-            running = victory2(board)
+            check = 1  
+            while check == 1:
+                if board[ligne2-1][colonne2-1] != "-":
+                    print("ceci a déja été joué\n")
+                    play3, play4 = check_input(user2).split()
+                    ligne2 = int(play3)
+                    colonne2 = int(play4)
+                else:
+                    board[ligne2-1][colonne2-1] = "O"
+                    check = 2
             if running == False:
                 ranking(user2)
                 break
