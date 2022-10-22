f29d76e26d5b67c18e8f36448e5d14b72ae824fe
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2022-10-23 01:48:06 +0200

    œ devient oe

--- pendu.py
+++ pendu.py
@@ -4,7 +4,7 @@ while True:
         dico = f.read().split()
         import random
         chosen_word = (random.choice(dico))
-        chosen_word = chosen_word.replace("é", "e").replace("è", "e").replace("ê","e").replace("î", "i").replace("â", "a").replace("ô", "o").replace("û", "u").replace("ë", "e").replace("ï", "i")
+        chosen_word = chosen_word.replace("é", "e").replace("è", "e").replace("ê","e").replace("î", "i").replace("â", "a").replace("ô", "o").replace("û", "u").replace("ë", "e").replace("ï", "i").replace("œ","o e")
         chosen_word = chosen_word.upper()
 
     crypted_word = ''
